dbd1fa207193954c7b7229221a7e1b67b26f5d42
Christopher Penrose <christopherpenrose@gmail.com>
2018-03-16 00:36:09 -0700

    set fork version to waffletower/drift "1.5.3".  Added bug fix from waffletower/clojure-tools "1.1.3" fork

--- project.clj
+++ project.clj
@@ -1,9 +1,8 @@
 ;; IMPORTANT: When bumping the version number here, be sure to bump it also in
-;; src/drift/drift_version.clj! 
-(defproject drift "1.5.3"
+;; src/drift/drift_version.clj!
+(defproject waffletower/drift "1.5.3"
   :description "Drift is a rails like migration framework for Clojure."
-  :dependencies [[clojure-tools "1.1.2"]
+  :dependencies [[waffletower/clojure-tools "1.1.3"]
                  [org.clojure/tools.logging "0.2.3"]]
   :profiles {:dev {:dependencies [[log4j/log4j "1.2.16"]]}}
-
   :aot [drift.listener-protocol drift.Drift])
